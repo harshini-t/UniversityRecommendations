35373a70dd7b9bff80191d2574b676cce8f3839e
Durga Srivatsan <durgasrivatsan@Durgas-MacBook-Pro.local>
2020-05-04 12:26:28 -0400

    made app a lot cleaner and added labels

--- src/App.java
+++ src/App.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -21,7 +22,7 @@ public class App implements Runnable {
 		JFrame frame = new JFrame("College Recommendations and Statistics!");
 		JPanel panel = new JPanel();
 		UniversityDataParser parser = new UniversityDataParser();
-		parser.getUniversityLinks();
+		/*parser.getUniversityLinks();
 		parser.getCountryRank();
         parser.getSchoolsInUniversity();
         parser.getFinancialAid();
@@ -31,7 +32,7 @@ public class App implements Runnable {
         parser.getCampusSetting();
         parser.getPublicPrivate();
         parser.getAddress();
-        parser.getAcademicSystem();
+        parser.getAcademicSystem();*/
 		
         frame.add(panel);
 		frame.setLocation(300, 300);
@@ -71,73 +72,140 @@ public class App implements Runnable {
             	String [] rank4 = {"Size", "Campus Setting", "Public/Private", "Academic Calendar", "Location"};
             	String [] rank5 = {"Size", "Campus Setting", "Public/Private", "Academic Calendar", "Location"};
             	
-            	final JComboBox<String> es = new JComboBox<String>(enrollmentSize);
-            	c.fill = GridBagConstraints.HORIZONTAL;
-        	    c.weightx = 0.5;
-        	    c.gridx = 0;
-        	    c.gridy = 0;
-        	    subPanel.add(es);
-            	final JComboBox<String> cs = new JComboBox<String>(campusSetting);
-            	c.fill = GridBagConstraints.HORIZONTAL;
-        	    c.weightx = 0.5;
-        	    c.gridx = 1;
-        	    c.gridy = 0;
-        	    subPanel.add(cs);
-            	final JComboBox<String> pp = new JComboBox<String>(publicPrivate);
-            	c.fill = GridBagConstraints.HORIZONTAL;
-        	    c.weightx = 0.5;
-        	    c.gridx = 2;
-        	    c.gridy = 0;
-        	    subPanel.add(pp);
-            	final JComboBox<String> as = new JComboBox<String>(academicCalendar);
-            	c.fill = GridBagConstraints.HORIZONTAL;
-        	    c.weightx = 0.5;
-        	    c.gridx = 3;
-        	    c.gridy = 0;
-        	    subPanel.add(as);
-            	final JTextField loc = new JTextField(20);
-            	c.fill = GridBagConstraints.HORIZONTAL;
-        	    c.weightx = 0.5;
-        	    c.gridx = 4;
-        	    c.gridy = 0;
-        	    subPanel.add(loc);
-            	final JComboBox<String> r1 = new JComboBox<String>(rank1);
-            	c.fill = GridBagConstraints.HORIZONTAL;
-        	    c.weightx = 0.5;
-        	    c.gridx = 5;
-        	    c.gridy = 0;
-        	    subPanel.add(r1);
-            	final JComboBox<String> r2 = new JComboBox<String>(rank2);
-            	c.fill = GridBagConstraints.HORIZONTAL;
-        	    c.weightx = 0.5;
-        	    c.gridx = 0;
-        	    c.gridy = 2;
-        	    subPanel.add(r2);
-            	final JComboBox<String> r3 = new JComboBox<String>(rank3);
-            	c.fill = GridBagConstraints.HORIZONTAL;
-        	    c.weightx = 0.5;
-        	    c.gridx = 1;
-        	    c.gridy = 2;
-        	    subPanel.add(r3);
-            	final JComboBox<String> r4 = new JComboBox<String>(rank4);
-            	c.fill = GridBagConstraints.HORIZONTAL;
-        	    c.weightx = 0.5;
-        	    c.gridx = 2;
-        	    c.gridy = 2;
-        	    subPanel.add(r4);
-            	final JComboBox<String> r5 = new JComboBox<String>(rank5);
-            	c.fill = GridBagConstraints.HORIZONTAL;
-        	    c.weightx = 0.5;
-        	    c.gridx = 3;
-        	    c.gridy = 2;
-        	    subPanel.add(r5);
+            	String instructions = "Enter your preferences and select \n"
+            			+ "which preferences are the most important to you to get a top 3 \n"
+            			+ "list of schools that match your criteria!";
             	
-            	final JButton ok = new JButton("OK");
-            	c.fill = GridBagConstraints.HORIZONTAL;
+            	JOptionPane.showConfirmDialog(frame, instructions, 
+	    				"Instructions: ", JOptionPane.PLAIN_MESSAGE); 
+	    	
+            	final JComboBox<String> es = new JComboBox<String>(enrollmentSize);
+            	c.fill = GridBagConstraints.VERTICAL;
+        	    c.weightx = 0.5;
+        	    c.gridx = 0;
+        	    c.gridy = 1;
+        	    final JLabel esL = new JLabel("Size Preference"); 
+        	    subPanel.add(esL, c);
+        	    c.fill = GridBagConstraints.VERTICAL;
+        	    c.weightx = 0.5;
+        	    c.gridx = 0;
+        	    c.gridy = 2;
+        	    subPanel.add(es, c);
+            	final JComboBox<String> cs = new JComboBox<String>(campusSetting);
+            	c.fill = GridBagConstraints.VERTICAL;
+        	    c.weightx = 0.5;
+        	    c.gridx = 1;
+        	    c.gridy = 1;
+        	    final JLabel csL = new JLabel("Campus Setting Preference"); 
+        	    subPanel.add(csL, c);
+        	    c.fill = GridBagConstraints.VERTICAL;
+        	    c.weightx = 0.5;
+        	    c.gridx = 1;
+        	    c.gridy = 2;
+        	    subPanel.add(cs, c);
+            	final JComboBox<String> pp = new JComboBox<String>(publicPrivate);
+            	c.fill = GridBagConstraints.VERTICAL;
+        	    c.weightx = 0.5;
+        	    c.gridx = 2;
+        	    c.gridy = 1;
+        	    final JLabel ppL = new JLabel("Public/Private Preference"); 
+        	    subPanel.add(ppL, c);
+        	    c.fill = GridBagConstraints.VERTICAL;
+        	    c.weightx = 0.5;
+        	    c.gridx = 2;
+        	    c.gridy = 2;
+        	    subPanel.add(pp, c);
+            	final JComboBox<String> as = new JComboBox<String>(academicCalendar);
+            	c.fill = GridBagConstraints.VERTICAL;
+        	    c.weightx = 0.5;
+        	    c.gridx = 3;
+        	    c.gridy = 1;
+        	    final JLabel asL = new JLabel("Academic Calendar Preference"); 
+        	    subPanel.add(asL, c);
+        	    c.fill = GridBagConstraints.VERTICAL;
+        	    c.weightx = 0.5;
+        	    c.gridx = 3;
+        	    c.gridy = 2;
+        	    subPanel.add(as, c);
+            	final JTextField loc = new JTextField(20);
+            	c.fill = GridBagConstraints.VERTICAL;
+        	    c.weightx = 0.5;
+        	    c.gridx = 4;
+        	    c.gridy = 1;
+        	    final JLabel locL = new JLabel("State Preference"); 
+        	    subPanel.add(locL, c);
+        	    c.fill = GridBagConstraints.VERTICAL;
         	    c.weightx = 0.5;
         	    c.gridx = 4;
         	    c.gridy = 2;
-        	    subPanel.add(ok);
+        	    subPanel.add(loc, c);
+            	final JComboBox<String> r1 = new JComboBox<String>(rank1);
+            	c.fill = GridBagConstraints.VERTICAL;
+        	    c.weightx = 0.5;
+        	    c.gridx = 0;
+        	    c.gridy = 3;
+        	    final JLabel r1L = new JLabel("Most Important Factor"); 
+        	    subPanel.add(r1L, c);
+        	    c.fill = GridBagConstraints.VERTICAL;
+        	    c.weightx = 0.5;
+        	    c.gridx = 0;
+        	    c.gridy = 4;
+        	    subPanel.add(r1, c);
+            	final JComboBox<String> r2 = new JComboBox<String>(rank2);
+            	c.fill = GridBagConstraints.VERTICAL;
+        	    c.weightx = 0.5;
+        	    c.gridx = 1;
+        	    c.gridy = 3;
+        	    final JLabel r2L = new JLabel("2nd Most Important Factor"); 
+        	    subPanel.add(r2L, c);
+        	    c.fill = GridBagConstraints.VERTICAL;
+        	    c.weightx = 0.5;
+        	    c.gridx = 1;
+        	    c.gridy = 4;
+        	    subPanel.add(r2, c);
+            	final JComboBox<String> r3 = new JComboBox<String>(rank3);
+            	c.fill = GridBagConstraints.VERTICAL;
+        	    c.weightx = 0.5;
+        	    c.gridx = 2;
+        	    c.gridy = 3;
+        	    final JLabel r3L = new JLabel("3rd Most Important Factor"); 
+        	    subPanel.add(r3L, c);
+        	    c.fill = GridBagConstraints.VERTICAL;
+        	    c.weightx = 0.5;
+        	    c.gridx = 2;
+        	    c.gridy = 4;
+        	    subPanel.add(r3, c);
+            	final JComboBox<String> r4 = new JComboBox<String>(rank4);
+            	c.fill = GridBagConstraints.VERTICAL;
+        	    c.weightx = 0.5;
+        	    c.gridx = 3;
+        	    c.gridy = 3;
+        	    final JLabel r4L = new JLabel("4th Most Important Factor"); 
+        	    subPanel.add(r4L, c);
+        	    c.fill = GridBagConstraints.VERTICAL;
+        	    c.weightx = 0.5;
+        	    c.gridx = 3;
+        	    c.gridy = 4;
+        	    subPanel.add(r4, c);
+            	final JComboBox<String> r5 = new JComboBox<String>(rank5);
+            	c.fill = GridBagConstraints.VERTICAL;
+        	    c.weightx = 0.5;
+        	    c.gridx = 4;
+        	    c.gridy = 3;
+        	    final JLabel r5L = new JLabel("Least Important Factor"); 
+        	    subPanel.add(r5L, c);
+        	    c.fill = GridBagConstraints.VERTICAL;
+        	    c.weightx = 0.5;
+        	    c.gridx = 4;
+        	    c.gridy = 4;
+        	    subPanel.add(r5, c);
+            	
+            	final JButton ok = new JButton("Submit Preferences");
+            	c.fill = GridBagConstraints.VERTICAL;
+        	    c.weightx = 0.5;
+        	    c.gridx = 2;
+        	    c.gridy = 8;
+        	    subPanel.add(ok, c);
 
 
         	    ok.addActionListener(new ActionListener() {
@@ -171,28 +239,28 @@ public class App implements Runnable {
 	    graph.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
 
-            	//subPanel.removeAll();
+            	JFrame subFrame = new JFrame("Statistics");
             	
-            	String [] questionChoices = {"Get Colleges in a Specific State: "};
-            	String [] campusSetting = {"hello"};
+            	JPanel subPanel = new JPanel();
+            	subFrame.add(subPanel);
+            	subFrame.setLocation(300, 300);
+        		subPanel.setLayout(new GridBagLayout());
+            	
+            	String [] questionChoices = {"Enter Choices HERE"};
 
             	//final JComboBox<String> es = new JComboBox<String>(choices);
-            	final JComboBox<String> cs = new JComboBox<String>(campusSetting);
-            	final JButton ok = new JButton("OK");
+            	//final JComboBox<String> cs = new JComboBox<String>(campusSetting);
+            	final JButton ok = new JButton("Select Question");
 
-
-            	//subPanel.add(es);
-            	//subPanel.add(cs);
-            	//subPanel.add(ok);
 
         	    ok.addActionListener(new ActionListener() {
         	    	public void actionPerformed(ActionEvent f) {
-        	    	//	System.out.println("es: " + es.getSelectedItem());
-        	    		System.out.println("cs: " + cs.getSelectedItem());
         	    	}
         	    });
-
+        	    subFrame.pack(); 
+                subFrame.setVisible(true);
             }
+            
         });
 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
